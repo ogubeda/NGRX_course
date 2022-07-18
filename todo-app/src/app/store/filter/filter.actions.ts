b27d6fa55bc5697a57734d070b27118f4e91262e
Asgeirr <asgeirr@AsgeirrPC.localdomain>
2022-07-18 09:33:12 +0200

    Creación de app 4

--- todo-app/src/app/store/filter/filter.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from "@ngrx/store"
-
-export type validFilters = 'all' | 'active' | 'completed'
-
-export const setFilter = createAction(
-  "[Filter] Set Filter",
-  props<{ filter: string }>()
-)
--- todo-app/src/app/store/filter/filter.reducer.ts
+++
@@ -1,9 +0,0 @@
-import { createReducer, on } from "@ngrx/store"
-import * as actions from "./filter.actions"
-
-export const initialState: string = 'all'
-
-export const filterReducer = createReducer(
-  initialState,
-  on(actions.setFilter, (state, { filter }) => filter),
-)
